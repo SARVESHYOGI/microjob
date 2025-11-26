b020d4b9f5f54f91e09598339f8f24871d39ca36
SARVESHYOGI <140194642+SARVESHYOGI@users.noreply.github.com>
2025-11-26 10:30:47 +0530

    frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,39 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import AllRequest from "./pages/request/AllRequest";
+import MyRequest from "./pages/request/MyRequest";
+import AddRequest from "./pages/request/AddRequest";
+import SpecificRequest from "./pages/request/SpecificRequest";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Chat from "./pages/Chat";
+import Notification from "./pages/Notification";
+import Admin from "./pages/Admin";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/requests" element={<AllRequest />} />
+        <Route path="/myrequests" element={<MyRequest />} />
+        <Route path="/addrequests" element={<AddRequest />} />
+        <Route path="/request/:id" element={<SpecificRequest />} />
+
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/register" element={<Register />} />
+
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/notifications" element={<Notification />} />
+
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile/admin" element={<Admin />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/request/AllRequest.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AllRequest() {
+  return (
+    <>
+      <div>AllRequest Page</div>
+    </>
+  );
+}
